7c27a0377cd50d1b0b6500bc61196f3f3ced9a14
lhaislla@gmail.com <lhaislla@gmail.com>
2024-09-24 09:35:58 -0300

    Sincronizando branch

--- index.py
+++ index.py
@@ -5,8 +5,6 @@ import pandas as pd
 from EggModelGen import EggModelGen
 from LoadImage import LoadImage
 from io import StringIO
-import random
-
 
 def load_key():
     _ = load_dotenv(find_dotenv())
@@ -17,11 +15,25 @@ def load_key():
 
 def create_instruction():
     prompt = """
-    Analise as imagens, e classifique usando a escala de 1 a 4, sendo 1 para uma qualidade  'muito boa' e 4 para 'muito ruim'.
-    O resultado deve ser em formato csv com as coluna do número do ovo e sua classificação
+    A ovoscopia é um método utilizado para avaliar a qualidade dos ovos, especialmente em processos de incubação e produção de ovos comerciais.
+        A técnica consiste em observar o interior do ovo por meio de uma luz intensa, geralmente em um ambiente escuro, para verificar a presença de anomalias, 
+        como fissuras na casca, problemas no desenvolvimento do embrião ou alterações na câmara de ar.
+
+        Você é uma zootecnista avícola especializada na avaliação de ovos. E irá classificar os ovos a partir de uma imagem provida como entrada no prompt.
+
+    Deve ser análise os seguintes pontos na imagem:
+    - Câmara de ar deslocada ou muito grande. No ovo fresco, a câmara de ar fica fixa em uma extremidade, pequena e estável.
+    - A presença de manchas escuras, pontos ou linhas dentro do ovo pode indicar a formação de mofo ou o desenvolvimento de bactérias, tornando-o impróprio para consumo.
+    - Se o interior do ovo aparece completamente escuro, sem a distinção das partes internas, é um sinal de contaminação ou deterioração.
+
+    A imagem conterá ovos dispostos verticalmente, com um total de 15 ovos. Classifique a qualidade da casca de cada ovo usando a escala de 1 a 4,
+    sendo 1 para uma qualidade  'muito boa' e 4 para 'muito ruim'. Os ovos dispostos horizontalmente estão vazios e não devem ser considerados.
+
+    Na imagem há 15 ovos distribuidos numa matriz com 3 linhas e 5 colunas. O resultado deve ser em formato csv com as coluna do número do ovo e sua classificação
     separada por ";". O número do ovo na ordem da baixo para cima da esquerda para direita.
-    Leve em consideração também a análise da ovoscopia;
-    O esquema de imagem seguida de um csv, servirá de exemplos de algumas classificações já existentes. Cada csv é a classificação correta ser retornado de acordo com a imagem dos ovos.
+
+    Como primeira instrução, a primeira imagem com um exemplo de referência da classificação de 1 a 4 de cada ovo. A imagem conterá 4 ovos dispostos horizontalmente, sendo o mais a esquerda classificado com 1 e o mais a direita classificado como 4. Para classificar os ovos veja qual que mais se aproxima da referência proposta. Leve em consideração também a análise da ovoscopia;
+    Logo após terá o esquema de uma imagem seguida de um csv, eles servirão de exemplos de algumas classificações já existentes. Cada csv é a classificação correta ser retornado de acordo com a imagem dos ovos anexada. Nesses casos, não precisa retornar nada pois de trata de casos de treinamento para você ter como referência.
     """
     return prompt
  
@@ -82,19 +94,14 @@ def result_to_dataframe(string_result):
 def load_all_egg_images_to_gemini(path_images):
 
     map_file_reference = {}
-
     i = 0
-
     for p,path in enumerate(path_images):
         start = i 
-
         if p%14 == 13:
             i += 5
         else:
             i += 15
-
         end = i
-
         file_gemini = load_image_to_gemini(path)
         map_file_reference[path] = [(start, end), file_gemini]
 
@@ -118,12 +125,11 @@ def load_training_images():
             if file_name.lower().endswith(('.jpg', '.jpeg', '.png')):
                 image_path = os.path.join(folder_path, file_name)
                 type_images_path.append(image_path)
-
     map_file_type_reference = load_all_egg_images_to_gemini(type_images_path)
     files_gemini_types = list(map(lambda x: x[1], map_file_type_reference.values()))
     LoadImage.wait_for_files_active(files_gemini_types)
-
     df_training_info = load_training_info() 
+    
     return map_file_type_reference, df_training_info  
 
 def load_classification_images(folders):
@@ -131,7 +137,7 @@ def load_classification_images(folders):
     map_file_reference = load_all_egg_images_to_gemini(images_eggs_path)
     files_gemini = list(map(lambda x: x[1], map_file_reference.values()))
     LoadImage.wait_for_files_active(files_gemini)
-    
+   
     return map_file_reference, files_gemini
 
 
@@ -155,10 +161,9 @@ def save_prompt_to_excel():
 def main():
     load_key()
     df = pd.read_excel("./IAGenOvoscopia/OVOSCOPIA-1RODADA - Sem 28dias.xlsx")
-    #folders = ["0 DIAS - FRESCOS",  "7 DIAS", "14 DIAS", "21 DIAS"]
-    folders = ["0 DIAS - FRESCOS"]
-
-    map_file_training_reference, df_training_info = load_training_images()  # Recebe as informações da planilha
+    folders = ["0 DIAS - FRESCOS",  "7 DIAS", "14 DIAS", "21 DIAS"]
+    #folders = ["0 DIAS - FRESCOS"]
+    map_file_training_reference, df_training_info = load_training_images()  
     training_images = [file_gemini for _, file_gemini in map_file_training_reference.values()]
     map_file_reference, files_gemini = load_classification_images(folders)
 
@@ -170,10 +175,9 @@ def main():
     for path in list(map_file_reference.keys()):
         example = map_file_reference[path]
         csv_example = generate_training_example(df, example[0][0], example[0][1])
-        examples.append(example[1])
         examples.append(csv_example)
         
-        if len(examples) >= 20:  
+        if len(examples) >= 14:  
             break
 
     chat_session = egg_ia_gen.model.start_chat(
